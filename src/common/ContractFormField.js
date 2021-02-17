114769582f677a47fa60196a7848c0e401e6723a
aarti singh <aarti.s@amazatic.com>
2021-02-17 18:13:45 +0530

    updated agreement side-pannel  dropdown.

--- src/common/ContractFormField.js
+++ src/common/ContractFormField.js
@@ -11,31 +11,8 @@ const ContractFormField = styled.div`
     text-transform: uppercase;
     font-weight: bold;
     
-
-     &.info-tool-icon {
-       position:relative;
-     }
   }
-  .copy-link {
-    position: absolute;
-    background: ${Theme.lightOrange};
-    color: ${Theme.orange};
-    border-radius: 4px;
-    font-size: ${Theme.small};
-    padding: 7px 10px;
-    right: 4px;
-    border-radius: 4px;
-    font-size: 13px;
-    padding: 7px 10px;
-    top: 44px;
-    font-weight:bold;
-    cursor: pointer;
-
-    &.without-info {
-      top: 44px;
-     right:30px;
-    }
-}
+ 
 
   .remove-field-icon {
     background: ${Theme.gray5};
@@ -60,20 +37,11 @@ const ContractFormField = styled.div`
     font-size:  ${Theme.small};
     font-weight: 600;
 
-    
-
-    &.info-tool{
-       margin-top: 20px;
-    }
 
     &.extra-space {
        padding: 4px 27px 4px 10px ;
     }
 
-    &.w-80 {
-      width: 85%;
-      
-    }
     &::placeholder {
       color: ${Theme.gray35};
       font-weight: 500;
@@ -82,95 +50,19 @@ const ContractFormField = styled.div`
 
     &:focus {
       outline: none;
+      border:1px solid ${Theme.gray25};
     }
   }
 
-   .text-area {
-      position:relative;
-      color: ${Theme.gray35};
-      border: 1px solid ${Theme.gray10};
-      border-radius: 5px;
-      padding: 4px  10px ;
-      display: block;
-      width: 100%;
-      background-color: rgba(224, 231, 255, 0.2);
-      margin-top: 5px;
-      font-family: ${Theme.baseFontFamily};
-      font-size:  ${Theme.title};
-      font-weight: 500;
-      resize: vertical;
-        &::placeholder {
-          color: ${Theme.gray35};
-          font-weight: 500;
-          font-size: color: ${Theme.normal};
-       }
-        &:focus {
-          outline: none;
-        }
-    } 
+
 
   .css-14jk2my-container {
     background-color: rgba(224, 231, 255, 0.2);
-    margin-top: 5px;
-    
+    margin-top: 5px;  
   }
-  .hide-icon {
-    position: absolute;
-    top: 50px;
-    right: 25px;
-  }
-  .show-icon {
-    position: absolute;
-    top: 50px;
-    right: 25px;
-  }
-  .hide-icons {
-    position: absolute;
-    top: 50px;
-    right: 114px;
-    cursor: pointer;
-  }
-  }
-  .show-icons {
-    position: absolute;
-    top: 34px;
-    right: 95px;
-    cursor: pointer;
-  }
-  .copy-icon {
-    width: 30px;
-    position: absolute;
-    right: 6%;
-    top: 27px;
-    cursor: pointer;
-  }
-  .info-icon {
-    width: 15px;
-    position: absolute;
-    right: auto;
-    cursor: pointer;
-    top: 0px;
-    margin-left: 10px;
-
-    &.amazon {
-      position: initial
-    }
-  }
-  .phone-input-info {
-    position: absolute;
-    top: 37px;
-    right: 10px;
-  }
-  .contact-input-info {
-    position: absolute;
-    top: 12px;
-    left: 180px;
-  }
-  .profilephone-input-info {
-    position: absolute;
-    top: 106px;
-    right: 15px;
-  }
+  
+  
+  
   
 `;
 export default ContractFormField;
--- src/common/ContractInputSelect.js
+++ src/common/ContractInputSelect.js
@@ -11,48 +11,50 @@ const ContractInputSelect = styled.div`
     text-transform: uppercase;
     font-weight: bold;
   }
-
-  .css-yk16xz-control {
-    color: ${Theme.gray90};
-    border: 1px solid ${Theme.gray10};
-    background-color: rgba(224, 231, 255, 0.2);
-    margin-top: 5px;
-    min-height: 32px !important;
-
-    .css-g1d714-ValueContainer {
-      color: ${Theme.gray35};
-      font-size: color: ${Theme.small};
-
-      .css-1wa3eu0-placeholder {
-        color: ${Theme.gray35};
-        font-weight: 500;
-        font-size: color: ${Theme.small};
-        text-transform: none;
-      }
-    }
-    .css-1hwfws3 {
-      font-size: color: ${Theme.small};
-    }
-
-    &:hover {
+  .css-2b097c-container {
+    .css-yk16xz-control {
+      color: ${Theme.gray90};
       border: 1px solid ${Theme.gray10};
-    }
-    
-    .react-select__option--is-focused:not(.react-select__option--is-selected){
-     .css-g1d714-ValueContainer { 
-       min-height:32px;
-     }
-    }
-  }
+      background-color: rgba(224, 231, 255, 0.2);
+      margin-top: 5px;
+      min-height: 32px !important;
+      font-size: color: ${Theme.small};
 
-  .css-1pahdxg-control {
-    box-shadow: none;
-    min-height:32px;
-    margin-top: 5px;
-    background-color: rgba(224, 231, 255, 0.2);
-    border: 1px solid ${Theme.gray10} !important;
-    outline: none;
-    font-size: ${Theme.small};
+        .css-g1d714-ValueContainer {
+          color: ${Theme.gray35};
+          font-size: color: ${Theme.small};
+        }
+          .css-1wa3eu0-placeholder {
+            color: ${Theme.gray35};
+            font-weight: 500;
+            font-size: color: ${Theme.small};
+            text-transform: none;
+          }
+          .css-tlfecz-indicatorContainer {
+            padding: 4px !important;
+          }
+      }
+
+    .css-1pahdxg-control {
+        box-shadow: none;
+        min-height:32px;
+        margin-top: 5px;
+        background-color: rgba(224, 231, 255, 0.2);
+        border: 1px solid ${Theme.gray10} !important;
+        outline: none;
+        font-size: ${Theme.small};
+
+        .css-1gtu0rj-indicatorContainer {
+          padding: 4px;
+        }
+    }
+    .css-1uccc91-singleValue {
+      font-size: ${Theme.small};
+    }
+    .css-1wa3eu0-placeholder {
+      font-size: ${Theme.small};
+    }
+      
   }
 `;
 
