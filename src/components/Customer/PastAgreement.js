b01d2fe9c9c4e4dfa945243e7839a8b22074d734
Aaditi Dhadwal <aaditi.d@amazatic.com>
2021-03-09 20:34:06 +0530

    past agreement bug fixed.

--- src/components/Customer/PastAgreement.js
+++ src/components/Customer/PastAgreement.js
@@ -25,73 +25,80 @@ export default function PastAgreement({ id }) {
         <div>
           {multipleAgreement &&
             multipleAgreement.map((item) => (
-              <WhiteCard className="mt-3" key={item.id}>
-                <div className="row">
-                  <div className="col-lg-9 col-md-8 col-12">
-                    <p className="black-heading-title mt-0 mb-0">
-                      {' '}
-                      <img
-                        className="solid-icon "
-                        src={
-                          item && item.contract_type === 'One Time'
-                            ? ServiceIcon
-                            : RecurringIcon
-                        }
-                        alt=""
-                      />{' '}
-                      {item && item.contract_type === 'One Time'
-                        ? 'One Time Services Contract'
-                        : 'Recurring Contract'}
-                    </p>
-                    <ul className="recurring-contact mb-2 ">
-                      <li>
-                        <p className="basic-text ">
-                          {item && item.length && item.length.label} contract
+              <React.Fragment key={item.id}>
+                {item.end_date < dayjs(new Date()).format('YYYY-MM-DD') ? (
+                  <WhiteCard className="mt-3">
+                    <div className="row">
+                      <div className="col-lg-9 col-md-8 col-12">
+                        <p className="black-heading-title mt-0 mb-0">
+                          {' '}
+                          <img
+                            className="solid-icon "
+                            src={
+                              item && item.contract_type === 'One Time'
+                                ? ServiceIcon
+                                : RecurringIcon
+                            }
+                            alt=""
+                          />{' '}
+                          {item && item.contract_type === 'One Time'
+                            ? 'One Time Services Contract'
+                            : 'Recurring Contract'}
                         </p>
-                      </li>
-                      {item && item.end_date ? (
-                        <li>
-                          <p className="basic-text ">
-                            Started:{' '}
-                            {dayjs(item.start_date).format('MMM DD, YYYY')}
-                          </p>
-                        </li>
-                      ) : (
-                        ''
-                      )}
-                      {item && item.start_date ? (
-                        <li>
-                          <p className="basic-text ">
-                            Expires:{' '}
-                            {dayjs(multipleAgreement.end_date).format(
-                              'MMM DD, YYYY',
-                            )}
-                          </p>
-                        </li>
-                      ) : (
-                        ''
-                      )}
-                    </ul>
-                  </div>
-                  <div className="col-lg-3 col-md-4 col-12 text-right">
-                    <Link
-                      to={{
-                        pathname: PATH_AGREEMENT.replace(':id', id),
-                        state: item.id,
-                      }}>
-                      <Button className="btn-transparent w-100  view-contract ">
-                        {' '}
-                        <img
-                          className="file-contract-icon"
-                          src={FileContract}
-                          alt=""
-                        />
-                        View Contract
-                      </Button>
-                    </Link>
-                  </div>
-                </div>
-              </WhiteCard>
+                        <ul className="recurring-contact mb-2 ">
+                          <li>
+                            <p className="basic-text ">
+                              {item && item.length && item.length.label}{' '}
+                              contract
+                            </p>
+                          </li>
+                          {item && item.start_date ? (
+                            <li>
+                              <p className="basic-text ">
+                                Started:{' '}
+                                {dayjs(item.start_date).format('MMM DD, YYYY')}
+                              </p>
+                            </li>
+                          ) : (
+                            ''
+                          )}
+                          {item && item.start_date ? (
+                            <li>
+                              <p className="basic-text ">
+                                Expires:{' '}
+                                {dayjs(item.end_date).format('MMM DD, YYYY')}
+                              </p>
+                            </li>
+                          ) : (
+                            ''
+                          )}
+                        </ul>
+                      </div>
+                      <div className="col-lg-3 col-md-4 col-12 text-right">
+                        <Link
+                          to={{
+                            pathname: PATH_AGREEMENT.replace(':id', id),
+                            state: item.id,
+                          }}>
+                          <Button className="btn-transparent w-100  view-contract ">
+                            {' '}
+                            <img
+                              className="file-contract-icon"
+                              src={FileContract}
+                              alt=""
+                            />
+                            View Contract
+                          </Button>
+                        </Link>
+                      </div>
+                    </div>
+                  </WhiteCard>
+                ) : (
+                  <WhiteCard className="text-center">
+                    No Past Agreements Found.
+                  </WhiteCard>
+                )}
+              </React.Fragment>
             ))}
         </div>
       ) : (
