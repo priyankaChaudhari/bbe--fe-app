61ea1d12ef133d977ae094988df7dd353dfe16d9
“priyankaChaudhari” <priyanka.c@amazatic.com>
2021-03-26 11:11:19 +0530

    PDV-736 ticket solved.

--- src/components/Contract/RequestSignature.js
+++ src/components/Contract/RequestSignature.js
@@ -59,8 +59,7 @@ function RequestSignature({
   const [formData, setFormData] = useState({});
   const [isLoading, setIsLoading] = useState({ loader: true, type: 'page' });
   const [selectedContact, setSelectedContact] = useState({});
-  const [ccEmails, setCCEmails] = useState([{}, {}]);
-  // const [ccEmails, setCCEmails] = useState([{}]);
+  const [ccEmails, setCCEmails] = useState([{}]);
   const [approvalNote, setApprovalNote] = useState({});
   const [sendContractCopy, setSendContractCopy] = useState(false);
   const [contractDesignData, setContractDesignData] = useState(null);
@@ -291,7 +290,7 @@ function RequestSignature({
   const displayCCEmails = () => {
     return ccEmails.map((data, i) => {
       return (
-        <div className="col-6 mt-2">
+        <div className="col-12 mt-3">
           <FormField>
             <input
               id={i}
@@ -379,7 +378,7 @@ function RequestSignature({
       contract: agreementData.id,
       to: selectedContact && selectedContact.email,
       cc: ccEmails,
-      // send_contract_copy: sendContractCopy,
+      send_contract_copy: sendContractCopy,
       contract_data: pdfData
         .replaceAll('PRINTED_NAME', mapValue('printed_name'))
         .replace('CUSTOMER_ROLE', mapValue('customer_role')),
@@ -501,7 +500,7 @@ function RequestSignature({
     <>
       {params && params.step === 'verify-document' ? (
         <>
-          {/* {contractDesignData && showHelloSignPage ? displayHelloSign() : ''} */}
+          {contractDesignData && showHelloSignPage ? displayHelloSign() : ''}
           <div className="modal-body ">
             <h4
               className="on-boarding mb-3"
@@ -525,15 +524,8 @@ function RequestSignature({
                   <label
                     className="check-container contract-sign"
                     htmlFor="contract-copy-check">
-                    <div className="send-copy-contract mt-4">
-                      Send me a copy of the contract
-                    </div>
-                    <div className="copy-review mt-2">
-                      Those in CC will get a copy of the contract for review.
-                    </div>
-
                     {/* <div className="send-copy-contract mt-4"> */}
-                    {/* Send me a copy of the contract */}
+                    Send me a copy of the contract
                     {/* </div> */}
                     <input
                       type="checkbox"
@@ -715,7 +707,7 @@ function RequestSignature({
       )}
       {params && params.step === 'select-contact' ? (
         <>
-          {contractDesignData && showHelloSignPage ? displayHelloSign() : ''}
+          {/* {contractDesignData && showHelloSignPage ? displayHelloSign() : ''} */}
           <div className="modal-body ">
             <h4 className="on-boarding mb-4">Select contact</h4>
             {isLoading.loader && isLoading.type === 'page' ? (
@@ -752,8 +744,8 @@ function RequestSignature({
             <div
               className="mt-4"
               role="presentation"
-              onClick={() => verifyDocument()}>
-              {/* // onClick={() => setParams('verify-document')}> */}
+              // onClick={() => verifyDocument()}>
+              onClick={() => setParams('verify-document')}>
               <Button
                 type="button"
                 disabled={!(selectedContact && selectedContact.id)}
