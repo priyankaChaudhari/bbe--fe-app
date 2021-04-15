77595c2058ad05f242b98d0f9c1739a11bcc2962
“priyankaChaudhari” <priyanka.c@amazatic.com>
2021-04-15 16:18:15 +0530

    past agreement bug fixed.

--- src/components/Customer/PastAgreement.js
+++ src/components/Customer/PastAgreement.js
@@ -20,86 +20,97 @@ export default function PastAgreement({ id }) {
     (state) => state.accountState.multipleAgreement,
   );
 
-  const generateHTML = (item) => {
+  const generateHTML = () => {
     const fields = [];
-    fields.push(
-      <WhiteCard className="mt-3">
-        <div className="row">
-          <div className="col-lg-9 col-md-8 col-12">
-            <p className="black-heading-title mt-0 mb-0">
-              {' '}
-              <img
-                width="48px"
-                className="solid-icon "
-                src={
-                  item && item.contract_type === 'One Time'
-                    ? ServiceIcon
-                    : item && item.contract_type === 'dsp only'
-                    ? DspOnlyIcon
-                    : RecurringIcon
-                }
-                alt=""
-              />{' '}
-              {(item && item.contract_type === 'One Time') ||
-              (item && item.contract_type === 'one time')
-                ? 'One Time Services Contract'
-                : item && item.contract_type === 'dsp only'
-                ? 'DSP Only Services Contract'
-                : 'Recurring Contract'}
-            </p>
-            <ul className="recurring-contact mb-2 ">
-              <li>
-                <p className="basic-text ">
-                  {item && item.length && item.length.label} contract
-                </p>
-              </li>
-              {item && item.start_date ? (
-                <li>
-                  <div className="dot" />
-                  <p className="basic-text ">
-                    Started: {dayjs(item.start_date).format('MMM DD, YYYY')}
-                  </p>
-                </li>
-              ) : (
-                ''
-              )}
-              {item && item.start_date ? (
-                <li>
-                  <p className="basic-text ">
-                    Expires: {dayjs(item.end_date).format('MMM DD, YYYY')}
-                  </p>
-                </li>
-              ) : (
-                ''
-              )}
-            </ul>
-          </div>
-          {item &&
+    for (const item of multipleAgreement) {
+      if (
+        (item &&
           item.contract_status &&
-          (item.contract_status.value === 'pending account setup' ||
-            item.contract_status.value === 'active') &&
-          item.contract_url === null ? null : (
-            <div className="col-lg-3 col-md-4 col-12 text-right">
-              <Link
-                to={{
-                  pathname: PATH_AGREEMENT.replace(':id', id),
-                  state: item.id,
-                }}>
-                <Button className="btn-transparent w-100  view-contract ">
+          item.contract_status.value &&
+          item.contract_status.value === 'inactive') ||
+        item.end_date > dayjs(new Date()).format('YYYY-MM-DD')
+      ) {
+        fields.push(
+          <WhiteCard className="mt-3" key={item.id}>
+            <div className="row">
+              <div className="col-lg-9 col-md-8 col-12">
+                <p className="black-heading-title mt-0 mb-0">
                   {' '}
                   <img
-                    className="file-contract-icon"
-                    src={FileContract}
+                    width="48px"
+                    className="solid-icon "
+                    src={
+                      item && item.contract_type === 'One Time'
+                        ? ServiceIcon
+                        : item && item.contract_type === 'dsp only'
+                        ? DspOnlyIcon
+                        : RecurringIcon
+                    }
                     alt=""
-                  />
-                  View Contract
-                </Button>
-              </Link>
+                  />{' '}
+                  {(item && item.contract_type === 'One Time') ||
+                  (item && item.contract_type === 'one time')
+                    ? 'One Time Services Contract'
+                    : item && item.contract_type === 'dsp only'
+                    ? 'DSP Only Services Contract'
+                    : 'Recurring Contract'}
+                </p>
+                <ul className="recurring-contact mb-2 ">
+                  <li>
+                    <p className="basic-text ">
+                      {item && item.length && item.length.label} contract
+                    </p>
+                  </li>
+                  {item && item.start_date ? (
+                    <li>
+                      <div className="dot" />
+                      <p className="basic-text ">
+                        Started: {dayjs(item.start_date).format('MMM DD, YYYY')}
+                      </p>
+                    </li>
+                  ) : (
+                    ''
+                  )}
+                  {item && item.start_date ? (
+                    <li>
+                      <p className="basic-text ">
+                        Expires: {dayjs(item.end_date).format('MMM DD, YYYY')}
+                      </p>
+                    </li>
+                  ) : (
+                    ''
+                  )}
+                </ul>
+              </div>
+              {item &&
+              item.contract_status &&
+              (item.contract_status.value === 'pending account setup' ||
+                item.contract_status.value === 'active') &&
+              item.contract_url === null ? null : (
+                <div className="col-lg-3 col-md-4 col-12 text-right">
+                  <Link
+                    to={{
+                      pathname: PATH_AGREEMENT.replace(':id', id),
+                      state: item.id,
+                    }}>
+                    <Button className="btn-transparent w-100  view-contract ">
+                      {' '}
+                      <img
+                        className="file-contract-icon"
+                        src={FileContract}
+                        alt=""
+                      />
+                      View Contract
+                    </Button>
+                  </Link>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </WhiteCard>,
-    );
+          </WhiteCard>,
+        );
+      }
+    }
+
     return fields && fields.length === 0 ? (
       <WhiteCard className="text-center">No Agreements Found.</WhiteCard>
     ) : (
@@ -111,16 +122,7 @@ export default function PastAgreement({ id }) {
     <>
       {multipleAgreement && multipleAgreement.length !== 0 ? (
         <div>
-          {multipleAgreement &&
-            multipleAgreement.map((item) => (
-              <React.Fragment key={item.id}>
-                {item.end_date < dayjs(new Date()).format('YYYY-MM-DD') ? (
-                  <>{generateHTML(item)}</>
-                ) : (
-                  ''
-                )}
-              </React.Fragment>
-            ))}
+          <>{generateHTML()}</>
         </div>
       ) : (
         <WhiteCard className="text-center">No Past Agreements Found.</WhiteCard>
