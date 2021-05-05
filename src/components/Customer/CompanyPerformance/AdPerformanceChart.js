c4b67276ede001e423ecb5d557379cdb6b9abd9e
Suraj Nalawade <suraj.n@amazatic.com>
2021-05-05 17:33:00 +0530

    pdv-969- bind tolltip label

--- src/components/Customer/CompanyPerformance/AdPerformanceChart.js
+++ src/components/Customer/CompanyPerformance/AdPerformanceChart.js
@@ -26,8 +26,8 @@ export default function AdPerformanceChart({
   //     adSalesPrevious: 4800,
   //     adSpendCurrent: 4000,
   //     adSpendPrevious: 3000,
-  //     adConversionCurrent: 5000,
-  //     adConversionPrevious: 2500,
+  //     adConversionCurrent: 50,
+  //     adConversionPrevious: 25,
   //     impressionsCurrent: 9000,
   //     impressionsPrevious: 1200,
   //     adCosCurrent: 4500,
@@ -46,8 +46,8 @@ export default function AdPerformanceChart({
   //     adSalesPrevious: 800,
   //     adSpendCurrent: 4700,
   //     adSpendPrevious: 3900,
-  //     adConversionCurrent: 7000,
-  //     adConversionPrevious: 5500,
+  //     adConversionCurrent: 70,
+  //     adConversionPrevious: 55,
   //     impressionsCurrent: 6000,
   //     impressionsPrevious: 8200,
   //     adCosCurrent: 4600,
@@ -66,8 +66,8 @@ export default function AdPerformanceChart({
   //     adSalesPrevious: 4810,
   //     adSpendCurrent: 4801,
   //     adSpendPrevious: 3011,
-  //     adConversionCurrent: 5000,
-  //     adConversionPrevious: 2500,
+  //     adConversionCurrent: 50,
+  //     adConversionPrevious: 25,
   //     impressionsCurrent: 9000,
   //     impressionsPrevious: 1200,
   //     adCosCurrent: 4700,
@@ -86,8 +86,8 @@ export default function AdPerformanceChart({
   //     adSalesPrevious: 1200,
   //     adSpendCurrent: 8760,
   //     adSpendPrevious: 4500,
-  //     adConversionCurrent: 6700,
-  //     adConversionPrevious: 2800,
+  //     adConversionCurrent: 67,
+  //     adConversionPrevious: 28,
   //     impressionsCurrent: 7000,
   //     impressionsPrevious: 4500,
   //     adCosCurrent: 8120,
@@ -106,8 +106,8 @@ export default function AdPerformanceChart({
   //     adSalesPrevious: 8300,
   //     adSpendCurrent: 2600,
   //     adSpendPrevious: 9200,
-  //     adConversionCurrent: 3000,
-  //     adConversionPrevious: 6500,
+  //     adConversionCurrent: 30,
+  //     adConversionPrevious: 65,
   //     impressionsCurrent: 2600,
   //     impressionsPrevious: 5700,
   //     adCosCurrent: 9100,
@@ -133,6 +133,17 @@ export default function AdPerformanceChart({
       adRoas: '#89A43C',
       adClicks: '#C84EC6',
       adClickRate: '#A04848',
+    };
+
+    const tooltipNames = {
+      adSales: 'AD SALES',
+      adSpend: 'AD SPEND',
+      adConversion: 'AD CONVERSION RATE',
+      impressions: 'IMPRESSIONS',
+      adCos: 'ACOS',
+      adRoas: 'ROAS',
+      adClicks: 'CLICKS',
+      adClickRate: 'CLICK THROUGH RATE',
     };
 
     chart.current = am4core.create(chartId, am4charts.XYChart);
@@ -187,7 +198,7 @@ export default function AdPerformanceChart({
         selectedKey === 'adConversion' ||
         selectedKey === 'adClickRate'
       ) {
-        valueAxis.numberFormatter.numberFormat = `#%`;
+        valueAxis.numberFormatter.numberFormat = `#.#`;
         tooltipCurrent = `[#2e384d]● [/]Recent [/]: {${currentValue}}%\n`;
         tooltipPrevious = `[#BFC5D2]● [/]Previous [/]: {${previousValue}}%`;
       } else {
@@ -247,9 +258,7 @@ export default function AdPerformanceChart({
       const snapToSeries = [];
       let tooltipValue = '';
       _.keys(selectedBox).map((item) => {
-        tooltipValue = `${tooltipValue} [${
-          colorSet[item]
-        }]● [/]${item.toUpperCase()}: {${item}Current}\n`;
+        tooltipValue = `${tooltipValue} [${colorSet[item]}]● [/]${tooltipNames[item]}:\t{${item}Current}\n`;
         return '';
       });
       _.keys(selectedBox).map((item) => {
@@ -268,254 +277,17 @@ export default function AdPerformanceChart({
         circleBullet.circle.fill = am4core.color(colorSet[item]);
         circleBullet.circle.strokeWidth = 1;
         circleBullet.circle.radius = 3;
-        circleBullet.fillOpacity = 0;
-        circleBullet.strokeOpacity = 0;
-        const bulletState2 = circleBullet.states.create('hover');
-        bulletState2.properties.fillOpacity = 1;
-        bulletState2.properties.strokeOpacity = 1;
+        // circleBullet.fillOpacity = 0;
+        // circleBullet.strokeOpacity = 0;
+        // const bulletState2 = circleBullet.states.create('hover');
+        // bulletState2.properties.fillOpacity = 1;
+        // bulletState2.properties.strokeOpacity = 1;
 
         snapToSeries.push(series);
         return '';
       });
       chart.current.cursor.snapToSeries = snapToSeries;
     }
-    // else {
-    //   console.log('many', _.keys(selectedBox));
-    //   var snapToSeries = [];
-    //   var tooltipValue = '';
-    //   _.keys(selectedBox).map((item) => {
-    //     console.log('key:',item);
-    //     console.log('color:',colorSet[item]);
-    //     const currentValue = `${item}Current`;
-    //     tooltipValue = `${tooltipValue} [${colorSet[item]}]● [/]${item.toUpperCase()}: {${currentValue}}\n`;
-    //   });
-
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adSales')) {
-    //     const adSalesSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adSalesSeries.dataFields.valueY = 'adSalesCurrent';
-    //     adSalesSeries.dataFields.dateX = 'date';
-    //     adSalesSeries.name = 'adSalesCurrent';
-    //     adSalesSeries.strokeWidth = 2;
-    //     // adSalesSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adSalesSeries.tooltipText = tooltipValue;
-    //     adSalesSeries.stroke = am4core.color(colorSet['adSales']);
-    //     adSalesSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adSalesSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adSales']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adSalesSeries];
-    //     snapToSeries.push(adSalesSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adSpend')) {
-    //     const adSpendSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adSpendSeries.dataFields.valueY = 'adSpendCurrent';
-    //     adSpendSeries.dataFields.dateX = 'date';
-    //     adSpendSeries.name = 'adSpendCurrent';
-    //     adSpendSeries.strokeWidth = 2;
-    //     // adSpendSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adSpendSeries.tooltipText = tooltipValue;
-    //     adSpendSeries.stroke = am4core.color(colorSet['adSpend']);
-    //     adSpendSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adSpendSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adSpend']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adSpendSeries];
-    //     snapToSeries.push(adSpendSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adConversion')) {
-    //     const adConversionSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adConversionSeries.dataFields.valueY = 'adConversionCurrent';
-    //     adConversionSeries.dataFields.dateX = 'date';
-    //     adConversionSeries.name = 'adConversionCurrent';
-    //     adConversionSeries.strokeWidth = 2;
-    //     // adConversionSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adConversionSeries.tooltipText = tooltipValue;
-    //     adConversionSeries.stroke = am4core.color(colorSet['adConversion']);
-    //     adConversionSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adConversionSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adConversion']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adConversionSeries];
-    //     snapToSeries.push(adConversionSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'impressions')) {
-    //     const impressionsSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     impressionsSeries.dataFields.valueY = 'impressionsCurrent';
-    //     impressionsSeries.dataFields.dateX = 'date';
-    //     impressionsSeries.name = 'impressionsCurrent';
-    //     impressionsSeries.strokeWidth = 2;
-    //     // impressionsSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     impressionsSeries.tooltipText = tooltipValue;
-    //     impressionsSeries.stroke = am4core.color(colorSet['impressions']);
-    //     impressionsSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = impressionsSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['impressions']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [impressionsSeries];
-    //     snapToSeries.push(impressionsSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adCos')) {
-    //     const adCosSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adCosSeries.dataFields.valueY = 'adCosCurrent';
-    //     adCosSeries.dataFields.dateX = 'date';
-    //     adCosSeries.name = 'adCosCurrent';
-    //     adCosSeries.strokeWidth = 2;
-    //     // adCosSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adCosSeries.tooltipText = tooltipValue;
-    //     adCosSeries.stroke = am4core.color(colorSet['adCos']);
-    //     adCosSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adCosSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adCos']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adCosSeries];
-    //     snapToSeries.push(adCosSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adRoas')) {
-    //     const adRoasSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adRoasSeries.dataFields.valueY = 'adRoasCurrent';
-    //     adRoasSeries.dataFields.dateX = 'date';
-    //     adRoasSeries.name = 'adRoasCurrent';
-    //     adRoasSeries.strokeWidth = 2;
-    //     // adRoasSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adRoasSeries.tooltipText = tooltipValue;
-    //     adRoasSeries.stroke = am4core.color(colorSet['adRoas']);
-    //     adRoasSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adRoasSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adRoas']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adRoasSeries];
-    //     snapToSeries.push(adRoasSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adClicks')) {
-    //     const adClicksSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adClicksSeries.dataFields.valueY = 'adClicksCurrent';
-    //     adClicksSeries.dataFields.dateX = 'date';
-    //     adClicksSeries.name = 'adClicksCurrent';
-    //     adClicksSeries.strokeWidth = 2;
-    //     // adClicksSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adClicksSeries.tooltipText = tooltipValue;
-    //     adClicksSeries.stroke = am4core.color(colorSet['adClicks']);
-    //     adClicksSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adClicksSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adClicks']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adClicksSeries];
-    //     snapToSeries.push(adClicksSeries);
-    //   }
-    //   if (Object.prototype.hasOwnProperty.call(selectedBox, 'adClickRate')) {
-    //     const adClickRateSeries = chart.current.series.push(
-    //       new am4charts.LineSeries(),
-    //     );
-    //     adClickRateSeries.dataFields.valueY = 'adClickRateCurrent';
-    //     adClickRateSeries.dataFields.dateX = 'date';
-    //     adClickRateSeries.name = 'adClickRateCurrent';
-    //     adClickRateSeries.strokeWidth = 2;
-    //     // adClickRateSeries.tooltipText = `${tooltipHeader}${tooltipCurrent}${tooltipPrevious}`; // render tooltip
-    //     adClickRateSeries.tooltipText = tooltipValue;
-    //     adClickRateSeries.stroke = am4core.color(colorSet['adClickRate']);
-    //     adClickRateSeries.fill = am4core.color('#2e384d');
-
-    //     const circleBullet = adClickRateSeries.bullets.push(
-    //       new am4charts.CircleBullet(),
-    //     );
-    //     circleBullet.circle.fill = am4core.color(colorSet['adClickRate']);
-    //     circleBullet.circle.strokeWidth = 1;
-    //     circleBullet.circle.radius = 3;
-    //     // circleBullet.fillOpacity = 0;
-    //     // circleBullet.strokeOpacity = 0;
-    //     // var bulletState2 = circleBullet.states.create('hover');
-    //     // bulletState2.properties.fillOpacity = 1;
-    //     // bulletState2.properties.strokeOpacity = 1;
-
-    //     // chart.current.cursor.snapToSeries = [adClickRateSeries];
-    //     snapToSeries.push(adClickRateSeries);
-    //   }
-    //   chart.current.cursor.snapToSeries = snapToSeries;
-    // }
 
     return () => chart.current && chart.current.dispose();
   }, [chartId, chartData, currencySymbol, selectedBox]);
