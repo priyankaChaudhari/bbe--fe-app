0a28c33da936667fa64b9069662cdc0bb2a7a9af
Aaditi Dhadwal <aaditi.d@amazatic.com>
2021-06-14 18:41:15 +0530

    PDV-1396 done.

--- src/components/BrandAssetGathering/DelegationUpload.js
+++ src/components/BrandAssetGathering/DelegationUpload.js
@@ -20,6 +20,7 @@ import {
 import {
   PATH_BRAND_ASSET,
   PATH_CUSTOMER_DETAILS,
+  PATH_THANKS,
   PATH_UNAUTHORIZED_BRAND_ASSET,
 } from '../../constants';
 import {
@@ -43,27 +44,35 @@ export default function UploadDelegation() {
   const params = queryString.parse(history.location.search);
 
   const brandAssetData = useCallback(() => {
+    setIsLoading({ loader: true, type: 'page' });
+
     getBrandAssetsDetail(brandId).then((response) => {
-      setData(response && response.data);
-      if (
-        (response && response.data && response.data.steps === undefined) ||
-        (response && response.data && response.data.steps === null)
-      ) {
-        setIfStepsNull(true);
+      if (response && response.status === 401) {
+        history.push({ pathname: PATH_THANKS, search: 'step=completed' });
+        setIsLoading({ loader: false, type: 'page' });
       } else {
-        setIfStepsNull(false);
-      }
-      if (
-        response &&
-        response.data &&
-        (response.data.re_assigned_email === null ||
-          response.data.re_assigned_email === '')
-      ) {
-        setIsChecked(false);
-        $('.checkboxes input:checkbox').prop('checked', false);
-      } else {
-        setIsChecked(true);
-        $('.checkboxes input:checkbox').prop('checked', true);
+        setData(response && response.data);
+        if (
+          (response && response.data && response.data.steps === undefined) ||
+          (response && response.data && response.data.steps === null)
+        ) {
+          setIfStepsNull(true);
+        } else {
+          setIfStepsNull(false);
+        }
+        if (
+          response &&
+          response.data &&
+          (response.data.re_assigned_email === null ||
+            response.data.re_assigned_email === '')
+        ) {
+          setIsChecked(false);
+          $('.checkboxes input:checkbox').prop('checked', false);
+        } else {
+          setIsChecked(true);
+          $('.checkboxes input:checkbox').prop('checked', true);
+        }
+        setIsLoading({ loader: false, type: 'page' });
       }
     });
   }, [brandId]);
@@ -164,174 +173,183 @@ export default function UploadDelegation() {
 
   return (
     <>
-      {params && params.key ? (
-        <UnauthorizedHeader />
+      {isLoading.loader && isLoading.type === 'page' ? (
+        <PageLoader color="#FF5933" type="page" />
       ) : (
-        <BackToStep>
-          {' '}
-          <div className="container-fluid">
-            {' '}
-            <div className="row">
-              <div className="col-12">
-                <div
-                  role="presentation"
-                  className="back-link"
-                  onClick={() =>
-                    history.push(PATH_CUSTOMER_DETAILS.replace(':id', id))
-                  }>
-                  <img
-                    src={LeftArrowIcon}
-                    alt="aarow-back"
-                    className="arrow-back-icon "
-                  />
-                  Back to Dashboard
-                </div>
-              </div>
-            </div>
-          </div>{' '}
-        </BackToStep>
-      )}
-      <OnBoardingBody className="grey-bg-asset pb-3">
-        <div className="white-card-base  pb-4 ">
-          {/* <UnauthorizedHeader />{' '} */}
-          <h3 className="page-heading ">Upload Your Brand Assets</h3>
+        <>
           {params && params.key ? (
-            ''
+            <UnauthorizedHeader />
           ) : (
-            <>
-              <CheckBox className="mt-1 mb-4">
-                <label
-                  className="check-container customer-pannel "
-                  htmlFor="step">
-                  Ask someone else to complete this section
-                  <input
-                    className="checkboxes"
-                    type="checkbox"
-                    id="step"
-                    readOnly
-                    checked={data && data.re_assigned_email}
-                    onChange={(event) =>
-                      event.target.checked
-                        ? updateInput()
-                        : data && data.re_assigned_email
-                        ? updateEmail()
-                        : setIsChecked(false)
-                    }
-                  />
-                  <span className="checkmark" />
-                </label>
-              </CheckBox>
-              {isChecked ? (
-                <fieldset className="shape-without-border w-430 mt-2">
-                  <ContractFormField>
-                    <label htmlFor="email">
-                      Assign to (email)
-                      <input
-                        className="form-control"
-                        style={{ opacity: isChecked && !editEmail ? 0.5 : '' }}
-                        defaultValue={data && data.re_assigned_email}
-                        onChange={(event) => {
-                          setFormData({ email: event.target.value });
-                          setApiError({ re_assigned_email: '' });
-                        }}
-                        readOnly={isChecked && !editEmail}
-                      />
-                    </label>
-                    <ErrorMsg>
-                      {apiError &&
-                        apiError.re_assigned_email &&
-                        apiError.re_assigned_email[0]}
-                    </ErrorMsg>
-                    {data && data.re_assigned_email ? (
-                      <span
-                        className="edit-field cursor"
-                        role="presentation"
-                        onClick={() => setEditEmail(true)}>
-                        <img
-                          className="edit-icon"
-                          src={EditOrangeIcon}
-                          alt="edit"
-                        />{' '}
-                        Edit email address
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                    {data && data.re_assigned_email ? (
-                      <p className="info-text-gray m-0 pt-2">
-                        We’ve emailed them a link to submit the information in
-                        this section.
-                      </p>
-                    ) : (
-                      <p className="info-text-gray m-0 pt-3 ">
-                        We’ll email them a link to submit the information in
-                        this section.
-                      </p>
-                    )}
-                    <Button
-                      className="btn-primary w-100  mt-3"
-                      onClick={() => sendEmail()}
-                      disabled={formData.email === ''}>
-                      {isLoading.loader && isLoading.type === 'email' ? (
-                        <PageLoader color="#fff" type="button" />
-                      ) : (
-                        'Send Email'
-                      )}
-                    </Button>
-                  </ContractFormField>
-                </fieldset>
-              ) : (
-                ''
-              )}
-            </>
-          )}
-          <div className="complete-steps mt-3">
-            You’ll need the following to complete this step:
-          </div>
-          <ul className="account-steps-check">
-            <li>
-              <img src={OrangeCheckMark} alt="check" />
-              Your brand logo in vector format (e.g. AI or EPS)
-            </li>
-            <li>
-              <img src={OrangeCheckMark} alt="check" />A brand guidelines or
-              style guide document
-            </li>
-            <li>
-              <img src={OrangeCheckMark} alt="check" />
-              Brand font files
-            </li>
-            <li>
-              <img src={OrangeCheckMark} alt="check" />
-              Iconography
-            </li>
-            <li>
-              <img src={OrangeCheckMark} alt="check" />
-              Any other brand material
-            </li>
-          </ul>
-          <Button
-            className="btn-primary w-100 mb-2"
-            onClick={() => redirectTO()}
-            disabled={
-              !history.location.pathname.includes(
-                '/assigned-choose-delegate/',
-              ) &&
-              isChecked &&
-              !(data && data.re_assigned_email)
-            }>
-            Continue
-          </Button>
-          <p className="info-text-gray security-lock text-center  mb-0">
-            <img width="16px" src={SecurityLock} alt="lock" />
-            <span>
+            <BackToStep>
               {' '}
-              All files will be stored securely and never shared with a 3rd
-              party.
-            </span>
-          </p>
-        </div>
-      </OnBoardingBody>
+              <div className="container-fluid">
+                {' '}
+                <div className="row">
+                  <div className="col-12">
+                    <div
+                      role="presentation"
+                      className="back-link"
+                      onClick={() =>
+                        history.push(PATH_CUSTOMER_DETAILS.replace(':id', id))
+                      }>
+                      <img
+                        src={LeftArrowIcon}
+                        alt="aarow-back"
+                        className="arrow-back-icon "
+                      />
+                      Back to Dashboard
+                    </div>
+                  </div>
+                </div>
+              </div>{' '}
+            </BackToStep>
+          )}
+
+          <OnBoardingBody className="grey-bg-asset pb-3">
+            <div className="white-card-base  pb-4 ">
+              {/* <UnauthorizedHeader />{' '} */}
+              <h3 className="page-heading ">Upload Your Brand Assets</h3>
+              {params && params.key ? (
+                ''
+              ) : (
+                <>
+                  <CheckBox className="mt-1 mb-4">
+                    <label
+                      className="check-container customer-pannel "
+                      htmlFor="step">
+                      Ask someone else to complete this section
+                      <input
+                        className="checkboxes"
+                        type="checkbox"
+                        id="step"
+                        readOnly
+                        checked={data && data.re_assigned_email}
+                        onChange={(event) =>
+                          event.target.checked
+                            ? updateInput()
+                            : data && data.re_assigned_email
+                            ? updateEmail()
+                            : setIsChecked(false)
+                        }
+                      />
+                      <span className="checkmark" />
+                    </label>
+                  </CheckBox>
+                  {isChecked ? (
+                    <fieldset className="shape-without-border w-430 mt-2">
+                      <ContractFormField>
+                        <label htmlFor="email">
+                          Assign to (email)
+                          <input
+                            className="form-control"
+                            style={{
+                              opacity: isChecked && !editEmail ? 0.5 : '',
+                            }}
+                            defaultValue={data && data.re_assigned_email}
+                            onChange={(event) => {
+                              setFormData({ email: event.target.value });
+                              setApiError({ re_assigned_email: '' });
+                            }}
+                            readOnly={isChecked && !editEmail}
+                          />
+                        </label>
+                        <ErrorMsg>
+                          {apiError &&
+                            apiError.re_assigned_email &&
+                            apiError.re_assigned_email[0]}
+                        </ErrorMsg>
+                        {data && data.re_assigned_email ? (
+                          <span
+                            className="edit-field cursor"
+                            role="presentation"
+                            onClick={() => setEditEmail(true)}>
+                            <img
+                              className="edit-icon"
+                              src={EditOrangeIcon}
+                              alt="edit"
+                            />{' '}
+                            Edit email address
+                          </span>
+                        ) : (
+                          ''
+                        )}
+                        {data && data.re_assigned_email ? (
+                          <p className="info-text-gray m-0 pt-2">
+                            We’ve emailed them a link to submit the information
+                            in this section.
+                          </p>
+                        ) : (
+                          <p className="info-text-gray m-0 pt-3 ">
+                            We’ll email them a link to submit the information in
+                            this section.
+                          </p>
+                        )}
+                        <Button
+                          className="btn-primary w-100  mt-3"
+                          onClick={() => sendEmail()}
+                          disabled={formData.email === ''}>
+                          {isLoading.loader && isLoading.type === 'email' ? (
+                            <PageLoader color="#fff" type="button" />
+                          ) : (
+                            'Send Email'
+                          )}
+                        </Button>
+                      </ContractFormField>
+                    </fieldset>
+                  ) : (
+                    ''
+                  )}
+                </>
+              )}
+              <div className="complete-steps mt-3">
+                You’ll need the following to complete this step:
+              </div>
+              <ul className="account-steps-check">
+                <li>
+                  <img src={OrangeCheckMark} alt="check" />
+                  Your brand logo in vector format (e.g. AI or EPS)
+                </li>
+                <li>
+                  <img src={OrangeCheckMark} alt="check" />A brand guidelines or
+                  style guide document
+                </li>
+                <li>
+                  <img src={OrangeCheckMark} alt="check" />
+                  Brand font files
+                </li>
+                <li>
+                  <img src={OrangeCheckMark} alt="check" />
+                  Iconography
+                </li>
+                <li>
+                  <img src={OrangeCheckMark} alt="check" />
+                  Any other brand material
+                </li>
+              </ul>
+              <Button
+                className="btn-primary w-100 mb-2"
+                onClick={() => redirectTO()}
+                disabled={
+                  !history.location.pathname.includes(
+                    '/assigned-choose-delegate/',
+                  ) &&
+                  isChecked &&
+                  !(data && data.re_assigned_email)
+                }>
+                Continue
+              </Button>
+              <p className="info-text-gray security-lock text-center  mb-0">
+                <img width="16px" src={SecurityLock} alt="lock" />
+                <span>
+                  {' '}
+                  All files will be stored securely and never shared with a 3rd
+                  party.
+                </span>
+              </p>
+            </div>
+          </OnBoardingBody>
+        </>
+      )}
     </>
   );
 }
