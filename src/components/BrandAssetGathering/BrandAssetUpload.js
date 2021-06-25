c67e3d46c190870dcd97357eed1d2668a5edd983
Aaditi Dhadwal <aaditi.d@amazatic.com>
2021-06-25 12:29:19 +0530

    PDV-1525, toaster added when files download.

--- src/components/BrandAssetGathering/BrandAssetUpload.js
+++ src/components/BrandAssetGathering/BrandAssetUpload.js
@@ -1124,6 +1124,7 @@ export default function BrandAssetUpload() {
                             }`}
                             target="_self"
                             onClick={() => {
+                              toast.success('Files will download shortly.');
                               setIsLoading({
                                 loader: true,
                                 type: 'button',
