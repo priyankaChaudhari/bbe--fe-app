f4d412afaebef4125578b869a22248c43332059e
aarti singh <aarti.s@amazatic.com>
2021-10-22 10:23:28 +0530

    pdv-2556 in progress .

--- src/common/ModalRadioCheck.js
+++ src/common/ModalRadioCheck.js
@@ -2,29 +2,27 @@ import styled from 'styled-components';
 import Theme from '../theme/Theme';
 
 const ModalRadioCheck = styled.div`
+  &.gray-bg {
+    background: ${Theme.gray8};
+    padding: 14px;
+    position: relative;
+    border-radius: 8px;
+    margin-bottom: 15px;
 
-&.gray-bg {
-  background:${Theme.gray8};
-  padding: 14px;
-  position: relative;
-  border-radius: 8px;
-  margin-bottom: 15px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .contact-billing {
+    font-size: 14px !important;
+    font-weight: 500;
+    color: ${Theme.gray85};
 
-  &:last-child {
-    margin-bottom:0;
+    .checkmark-top {
+      top: 0px;
+    }
   }
 
-}
-.contact-billing {
-  font-size: 14px !important;
-  font-weight: 500;
-  color: ${Theme.gray85};
-
-  .checkmark-top {
-     top: 0px;  
-   }
-}
-  
   .radio-container {
     display: block;
     position: relative;
@@ -33,33 +31,32 @@ const ModalRadioCheck = styled.div`
     cursor: pointer;
     color: ${Theme.gray90};
     font-family: ${Theme.baseFontFamily};
-    font-size ${Theme.medium};
+    font-size: ${Theme.medium};
     -webkit-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
     user-select: none;
 
     &.customer-list {
-     color:${Theme.gray85};
-     font-size: 14px;
+      color: ${Theme.gray85};
+      font-size: 14px;
     }
-   .checkmark-customer-list {
-     top: 0px;  
-   }
-    
+    .checkmark-customer-list {
+      top: 0px;
+    }
   }
 
   .delete-contact {
     width: 16px;
     right: 14px;
     position: absolute;
-    top: 10px; 
+    top: 10px;
   }
   .edit-contact {
     width: 12px;
     right: 55px;
     position: absolute;
-    top: 12px
+    top: 12px;
   }
   span.owner-details {
     font-size: 13px;
@@ -115,8 +112,6 @@ const ModalRadioCheck = styled.div`
     border-radius: 50%;
     background: white;
   }
-  
- 
 `;
 
 export default ModalRadioCheck;
