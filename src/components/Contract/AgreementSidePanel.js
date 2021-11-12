86febab6527126afc46274e707f1ebce51b1608c
Shashank <shashank.n@amazatic.com>
2021-11-12 13:32:05 +0530

    Only start date of agreemnt is enabled in calendar.

--- src/components/Contract/AgreementSidePanel.js
+++ src/components/Contract/AgreementSidePanel.js
@@ -1618,6 +1618,12 @@ export default function AgreementSidePanel({
     return '';
   };
 
+  const tileDisabled = (date, agreementStartDate) => {
+    return (
+      dayjs(date.date).get('date') !== dayjs(agreementStartDate).get('date')
+    );
+  };
+
   const generateHTML = (item) => {
     if (item.type.includes('number')) {
       return (
@@ -1688,7 +1694,8 @@ export default function AgreementSidePanel({
           monthPlaceholder="MM"
           yearPlaceholder="YYYY"
           placeholderText="Select Date"
-          disabled={formData && formData.draft_from}
+          tileDisabled={(date) => tileDisabled(date, agreementData?.start_date)}
+          // disabled={formData && formData.draft_from}
         />
       );
     }
