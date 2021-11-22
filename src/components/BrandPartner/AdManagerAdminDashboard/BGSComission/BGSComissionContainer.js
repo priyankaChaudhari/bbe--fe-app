1ea3f309d76c558b75e69e258b61b96933e05fce
Shashank <shashank.n@amazatic.com>
2021-11-22 23:58:01 +0530

    [New]: Added CSV Creator package.

--- src/components/BrandPartner/AdManagerAdminDashboard/BGSComission/BGSComissionContainer.js
+++ src/components/BrandPartner/AdManagerAdminDashboard/BGSComission/BGSComissionContainer.js
@@ -73,6 +73,12 @@ const BGSComissionContainer = ({
     { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
   ];
 
+  const csvData = [
+    { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+    { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+    { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
+  ];
+
   const handleApply = () => {
     if (dummayDateRange !== null) {
       setState(dummayDateRange);
