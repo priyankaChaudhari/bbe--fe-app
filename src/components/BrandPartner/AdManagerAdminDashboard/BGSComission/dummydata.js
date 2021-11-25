6b7d3792c8eb97d78dcb6548900bf2229511cf86
Pradip Bade <badepradip123@gmail.com>
2021-11-25 15:39:39 +0530

    Integrated commission high level metrics with dummyData

---
+++ src/components/BrandPartner/AdManagerAdminDashboard/BGSComission/dummydata.js
@@ -0,0 +1,90 @@
+export const matrics = {
+  total_book_size: 1200000.65,
+  total_book_size_commission: 2445455,
+  total_upsells: 353333,
+  total_upselles_commission: 66666,
+};
+
+export const commissionGroupBy = {
+  count: 1023,
+  next: 'https://api.example.org/accounts/?page=5',
+  previous: 'https://api.example.org/accounts/?page=3',
+  results: [
+    {
+      members: [
+        {
+          id: 'asd',
+          name: 'Team Jake',
+          rev_share: '',
+          retainer: '',
+        },
+        {
+          id: 'asd',
+          name: 'Team Jake',
+          rev_share: '',
+          retainer: '',
+        },
+      ],
+      bgs_manager: {
+        id: 'asd',
+        name: 'Team Jake',
+        rev_share: '',
+        retainer: '',
+      },
+    },
+    {
+      members: [
+        {
+          id: 'asd',
+          name: 'Team Jake',
+          rev_share: '',
+          retainer: '',
+        },
+        {
+          id: 'asd',
+          name: 'Team Jake',
+          rev_share: '',
+          retainer: '',
+        },
+      ],
+      bgs_manager: {
+        id: 'asd',
+        name: 'Team Jake',
+        rev_share: '',
+        retainer: '',
+      },
+    },
+  ],
+};
+
+export const bgsCommissionList = {
+  count: 1023,
+  next: 'https://api.example.org/accounts/?page=5',
+  previous: 'https://api.example.org/accounts/?page=3',
+  results: [
+    {
+      id: 'asd',
+      name: 'Team Jake',
+      rev_share: '',
+      retainer: '',
+    },
+    {
+      id: 'asd',
+      name: 'Team Jake',
+      rev_share: '',
+      retainer: '',
+    },
+    {
+      id: 'asd',
+      name: 'Team Jake',
+      rev_share: '',
+      retainer: '',
+    },
+    {
+      id: 'asd',
+      name: 'Team Jake',
+      rev_share: '',
+      retainer: '',
+    },
+  ],
+};
