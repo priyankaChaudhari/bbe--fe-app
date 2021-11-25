e483028543d91c9c684205e0b70a9c566a8c1c3f
Shashank <shashank.n@amazatic.com>
2021-11-25 18:33:02 +0530

    [New]: Brand Partner overlay integrated with mock data.

--- src/components/BrandPartner/AdManagerAdminDashboard/BGSComission/dummydata.js
+++ src/components/BrandPartner/AdManagerAdminDashboard/BGSComission/dummydata.js
@@ -88,3 +88,54 @@ export const bgsCommissionList = {
     },
   ],
 };
+
+export const brandPartners = [
+  {
+    id: 1,
+    team_member: 'Ripley Tools LLC',
+    retainer: '$21000',
+    rev_share: '$2,193',
+    dsp: '0',
+    total_book_size: '$23,193',
+    book_size_commission: '$701.49',
+    upsells: '$1,050',
+    upsells_commission: '$105',
+    total_commission: '$806',
+  },
+  {
+    id: 2,
+    team_member: 'Ripley Tools LLC',
+    retainer: '$21000',
+    rev_share: '$2,193',
+    dsp: '0',
+    total_book_size: '$23,193',
+    book_size_commission: '$701.49',
+    upsells: '$1,050',
+    upsells_commission: '$105',
+    total_commission: '$806',
+  },
+  {
+    id: 3,
+    team_member: 'Ripley Tools LLC',
+    retainer: '$21000',
+    rev_share: '$2,193',
+    dsp: '0',
+    total_book_size: '$23,193',
+    book_size_commission: '$701.49',
+    upsells: '$1,050',
+    upsells_commission: '$105',
+    total_commission: '$806',
+  },
+];
+
+export const totalOfPartner = {
+  team_member: 'All Partners',
+  retainer: '$21000',
+  rev_share: '$2,193',
+  dsp: '0',
+  total_book_size: '$23,193',
+  book_size_commission: '$701.49',
+  upsells: '$1,050',
+  upsells_commission: '$105',
+  total_commission: '$1111',
+};
