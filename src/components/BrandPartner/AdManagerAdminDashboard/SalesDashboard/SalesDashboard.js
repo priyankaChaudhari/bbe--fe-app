cfada490c52370aaaba24be2629d319900ebfda9
Suraj Nalawade <suraj.n@amazatic.com>
2021-12-07 13:41:21 +0530

    fixed PDV- 3123- adding comma in graph tooltip, fixed PDV-3145

--- src/components/BrandPartner/AdManagerAdminDashboard/SalesDashboard/SalesDashboard.js
+++ src/components/BrandPartner/AdManagerAdminDashboard/SalesDashboard/SalesDashboard.js
@@ -181,7 +181,7 @@ export default function SalesDashboard({ marketplaceChoices, userInfo }) {
           trafficPreviousLabel:
             item.traffic !== null ? item.traffic.toFixed(2) : '0.00',
           conversionPreviousLabel:
-            item.conversion !== null ? item.conversion : '0',
+            item.conversion !== null ? item.conversion.toFixed(2) : '0',
         });
       });
     }
@@ -190,7 +190,6 @@ export default function SalesDashboard({ marketplaceChoices, userInfo }) {
     if (response.current && response.current.length) {
       response.current.forEach((item, index) => {
         const currentReportDate = dayjs(item.report_date).format('MMM D YYYY');
-        let indexNumber = index;
         // add the current data at same index of prevoius in temporary object
         if (response.previous && index < response.previous.length) {
           tempData[index].date = currentReportDate;
@@ -206,22 +205,7 @@ export default function SalesDashboard({ marketplaceChoices, userInfo }) {
           tempData[index].trafficCurrentLabel =
             item.traffic !== null ? item.traffic.toFixed(2) : '0.00';
           tempData[index].conversionCurrentLabel =
-            item.conversion !== null ? item.conversion : '0';
-
-          // to add the dotted line. we have to check null matrix and add the dummy number like 8
-          if (index > 0) {
-            indexNumber = index - 1;
-          } else {
-            indexNumber = index;
-          }
-          tempData[indexNumber].revenueDashLength =
-            item.revenue === null ? 8 : null;
-          tempData[indexNumber].unitsSoldDashLength =
-            item.units_sold === null ? 8 : null;
-          tempData[indexNumber].trafficDashLength =
-            item.traffic === null ? 8 : null;
-          tempData[indexNumber].conversionDashLength =
-            item.conversion === null ? 8 : null;
+            item.conversion !== null ? item.conversion.toFixed(2) : '0';
         } else {
           // if current data count is larger than previous count then
           // generate separate key for current data and defien previous value null and previous label 0
@@ -244,7 +228,7 @@ export default function SalesDashboard({ marketplaceChoices, userInfo }) {
             trafficCurrentLabel:
               item.traffic !== null ? item.traffic.toFixed(2) : '0.00',
             conversionCurrentLabel:
-              item.conversion !== null ? item.conversion : '0',
+              item.conversion !== null ? item.conversion.toFixed(2) : '0',
 
             revenuePreviousLabel: '0.00',
             unitsSoldPreviousLabel: '0.00',
