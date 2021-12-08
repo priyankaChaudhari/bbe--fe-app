2d80c283571b5f7c22dc6e35cce696438b00f20c
Pradip Bade <badepradip123@gmail.com>
2021-12-08 09:45:44 +0530

    Added InvoicePause & InvoiceAdjust comp in InvoiceAdjustmentModal.

--- src/components/Customer/CustomerDetailView/BillingContainer/Invoice/InvoiceAdjustmentModals/InvoiceAdjustPauseModal.js
+++ src/components/Customer/CustomerDetailView/BillingContainer/Invoice/InvoiceAdjustmentModals/InvoiceAdjustPauseModal.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Modal from 'react-modal';
 import Select from 'react-select';
 import { bool, func, shape, string } from 'prop-types';
-import styled from 'styled-components';
 
 import InvoiceAdjustDetailsModal from './InvoiceAdjustDetailsModal';
 import { CloseIcon } from '../../../../../../theme/images';
@@ -13,8 +12,10 @@ import {
   Tabs,
   ModalRadioCheck,
   ContractInputSelect,
-  InputFormField,
 } from '../../../../../../common';
+import { adjustInvoiceChoices } from '../../../../../../constants/CustomerConstants';
+import InvoiceAdjust from './InvoiceAdjust';
+import InvoicePause from './InvoicePause';
 
 const todaysDate = new Date();
 todaysDate.setDate(todaysDate.getDate() - 2);
@@ -42,6 +43,28 @@ const InvoiceAdjustPauseModal = ({
   onApply,
 }) => {
   const [showInvoiceDetailsModal, setShowInvoiceDetailsModal] = useState(false);
+  const [newAmount, setNewAmount] = useState({});
+  const [invoiceType, setInvoiceType] = useState('standard');
+  const [viewComponent, setViewComponent] = useState('adjustInvoice');
+
+  useEffect(() => {
+    console.log('new amount----', newAmount);
+  }, [newAmount]);
+
+  const returnTotalAmount = () => {
+    const amounts = Object.values(newAmount);
+    if (amounts && amounts.length > 0) {
+      let temp = 0;
+      amounts.forEach((value) => {
+        temp += isNaN(parseFloat(value.replace(/,/g, '')))
+          ? 0
+          : parseFloat(value.replace(/,/g, ''));
+      });
+      return temp;
+    }
+    return 0;
+  };
+
   return (
     <>
       <Modal
@@ -63,168 +86,66 @@ const InvoiceAdjustPauseModal = ({
             <Tabs className="mt-3">
               {' '}
               <ul className="tabs">
-                <li className="modal-tab active" role="presentation">
+                <li
+                  className={`modal-tab ${
+                    viewComponent === 'adjustInvoice' ? 'active' : ''
+                  }`}
+                  role="presentation"
+                  onClick={() => {
+                    setViewComponent('adjustInvoice');
+                  }}>
                   Adjust Invoice
                 </li>
-                <li className="modal-tab " role="presentation">
+                <li
+                  className={`modal-tab ${
+                    viewComponent === 'pauseInvoice' ? 'active' : ''
+                  }`}
+                  role="presentation"
+                  onClick={() => {
+                    setViewComponent('pauseInvoice');
+                  }}>
                   Pause Invoice
                 </li>
               </ul>
             </Tabs>
-            <ul className="invoice-adj-radio mt-4">
-              <li>
-                <ModalRadioCheck className="mb-3">
-                  {' '}
-                  <label
-                    className=" checkboxes radio-container customer-list"
-                    htmlFor="standard">
-                    <input type="radio" name="radio" id="standard" />
-                    <span className="checkmark checkmark-customer-list" />
-                    Standard
-                  </label>
-                </ModalRadioCheck>
-              </li>
-              <li>
-                <ModalRadioCheck className="mb-3">
-                  {' '}
-                  <label
-                    className=" checkboxes radio-container customer-list"
-                    htmlFor="standard">
-                    <input type="radio" name="radio" id="standard" />
-                    <span className="checkmark checkmark-customer-list" />
-                    Permanent Additional
-                  </label>
-                </ModalRadioCheck>
-              </li>
-              <li>
-                <ModalRadioCheck className="mb-3">
-                  {' '}
-                  <label
-                    className=" checkboxes radio-container customer-list"
-                    htmlFor="standard">
-                    <input type="radio" name="radio" id="standard" />
-                    <span className="checkmark checkmark-customer-list" />
-                    One Time
-                  </label>
-                </ModalRadioCheck>
-              </li>
-            </ul>
+            {viewComponent === 'adjustInvoice' ? (
+              <ul className="invoice-adj-radio mt-4">
+                {adjustInvoiceChoices.map((item) => {
+                  return (
+                    <li>
+                      <ModalRadioCheck className="mb-3">
+                        <label
+                          className=" checkboxes radio-container customer-list"
+                          htmlFor={item.id}>
+                          <input
+                            type="radio"
+                            name={item.name}
+                            checked={invoiceType === item.name}
+                            onChange={(e) => {
+                              setInvoiceType(e.target.name);
+                            }}
+                            id={item.id}
+                          />
+                          <span className="checkmark checkmark-customer-list" />
+                          {item.label}
+                        </label>
+                      </ModalRadioCheck>
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : null}
           </div>
 
-          <GrayTable>
-            <div className="modal-body pb-3">
-              <div className="row">
-                <div className="col-4 text-left">
-                  <div className="label">Marketplace</div>
-                </div>
-                <div className="col-4 text-left">
-                  <div className="label">Current</div>
-                </div>
-                <div className="col-4 text-left">
-                  <div className="label">New amount</div>
-                </div>
-
-                <div className=" straight-line horizontal-line pt-1 mb-2 " />
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">US</div>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">$5,000</div>
-                </div>
-                <div className="col-4 text-left">
-                  {/* <InputFormField id="BT-InvoiceAdjustPause">
-                    <div className="input-container  ">
-                      <span className="input-icon ">$</span>
-
-                      <input
-                        className="form-control"
-                        type="password"
-                        name="password"
-                        placeholder="Enter your Password"
-                        id="password"
-                      />
-                    </div>
-                  </InputFormField> */}
-                  <ul className="invoice-adj-radio mt-4">
-                    <li>
-                      <ModalRadioCheck className="mb-3">
-                        {' '}
-                        <label
-                          className=" checkboxes radio-container customer-list"
-                          htmlFor="yes">
-                          <input type="radio" name="radio" id="yes" />
-                          <span className="checkmark checkmark-customer-list" />
-                          Yes
-                        </label>
-                      </ModalRadioCheck>
-                    </li>
-                    <li>
-                      <ModalRadioCheck className="mb-3">
-                        {' '}
-                        <label
-                          className=" checkboxes radio-container customer-list"
-                          htmlFor="no">
-                          <input type="radio" name="radio" id="no" />
-                          <span className="checkmark checkmark-customer-list" />
-                          No
-                        </label>
-                      </ModalRadioCheck>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">UK</div>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">$5,000</div>
-                </div>
-                <div className="col-4 text-left">
-                  <InputFormField id="BT-escrow-numberFormat-budgetAllocaion">
-                    <div className="input-container  ">
-                      <span className="input-icon ">$</span>
-
-                      <input
-                        className="form-control"
-                        type="password"
-                        name="password"
-                        id="password"
-                      />
-                    </div>
-                  </InputFormField>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">Canada</div>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text ">$5,000</div>
-                </div>
-                <div className="col-4 text-left">
-                  <InputFormField id="BT-escrow-numberFormat-budgetAllocaion">
-                    <div className="input-container  ">
-                      <span className="input-icon ">$</span>
-
-                      <input
-                        className="form-control"
-                        type="password"
-                        name="password"
-                        id="password"
-                      />
-                    </div>
-                  </InputFormField>
-                </div>
-                <div className=" straight-line horizontal-line pt-2 " />
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text text-bold ">Total</div>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text text-bold">$10,000</div>
-                </div>
-                <div className="col-4 text-left mt-3">
-                  <div className="normal-text text-bold">$10,000 </div>
-                </div>
-              </div>
-            </div>
-          </GrayTable>
+          {viewComponent === 'adjustInvoice' ? (
+            <InvoiceAdjust
+              newAmount={newAmount}
+              setNewAmount={setNewAmount}
+              returnTotalAmount={returnTotalAmount}
+            />
+          ) : (
+            <InvoicePause returnTotalAmount={returnTotalAmount} />
+          )}
           <div className="modal-body pb-1 pt-3">
             <ContractInputSelect>
               <label htmlFor="amount">applies from </label>
@@ -279,7 +200,3 @@ InvoiceAdjustPauseModal.propTypes = {
   onModalClose: func,
   onApply: func,
 };
-
-const GrayTable = styled.div`
-  background-color: #f4f6fc;
-`;
