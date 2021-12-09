443c13bde10b23b34ed9bfcba2a888a519abd5f0
Suraj Nalawade <suraj.n@amazatic.com>
2021-12-09 11:08:03 +0530

    dsp sign off- added tooltip, send reminder modal, past adjustment modal

---
+++ src/components/Customer/CustomerDetailView/BillingContainer/Invoice/InvoiceAdjustmentModals/InvoiceViewAndReminderModal.js
@@ -0,0 +1,334 @@
+import React from 'react';
+
+import Modal from 'react-modal';
+import { useMediaQuery } from 'react-responsive';
+import { bool, func, shape, string } from 'prop-types';
+
+import { ModalBox, Button } from '../../../../../../common';
+import {
+  ArrowRightBlackIcon,
+  ArrowRightIcon,
+  CloseIcon,
+  LeftArrowIcon,
+  BellNotification,
+} from '../../../../../../theme/images';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    maxWidth: '600px ',
+    width: '100% ',
+    overlay: ' {zIndex: 1000}',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const InvoiceViewAndReminderModal = ({
+  id,
+  isOpen,
+  style,
+  onClick,
+  onApply,
+}) => {
+  const marketplaceData = [
+    {
+      name: 'UK',
+      from: 1000,
+      to: 5000,
+      change: 4000,
+    },
+    {
+      name: 'Canada',
+      from: 10000,
+      to: 6000,
+      change: 4000,
+    },
+    {
+      name: 'US',
+      from: 10000,
+      to: 15000,
+      change: 5000,
+    },
+  ];
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  const generateAmount = (value, valueFor, currencySymbol) => {
+    if (value && value !== null && value !== 0) {
+      value = Number(value.toFixed(2));
+      return `${
+        value < 0 ? '-' : valueFor === 'change' ? '+' : ''
+      }${currencySymbol}${value
+        .toString()
+        .replace('-', '')
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    }
+
+    return valueFor === 'change' ? '-' : 0;
+  };
+
+  const renderDesktopView = () => {
+    return (
+      <div className="d-md-block d-none">
+        <div className="row">
+          <div className="col-4 text-left">
+            <div className="label">Marketplace</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">From</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="label">To</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">Change</div>
+          </div>
+        </div>
+        <div className=" straight-line horizontal-line pt-1 mb-2 " />
+        {marketplaceData.map((item, index) => (
+          <div className={index ? 'row mt-1' : 'row'}>
+            <div className="col-4 text-left">
+              <div className="normal-text text-bold">{item.name}</div>
+            </div>
+            <div className="col-2 text-left">
+              <div className="normal-text text-bold">
+                {generateAmount(item?.from, 'from', '$')}
+              </div>
+            </div>
+            <div className="col-1 text-left">
+              <div className="normal-text">
+                <img src={ArrowRightBlackIcon} width="18px" alt="arrow" />{' '}
+              </div>
+            </div>
+            <div className="col-2 text-left">
+              <div className="normal-text text-bold">
+                {generateAmount(item?.to, 'to', '$')}
+              </div>
+            </div>
+            <div className="col-3 text-left">
+              <div className="normal-text text-bold">
+                {generateAmount(item?.change, 'change', '$')}
+              </div>
+            </div>
+          </div>
+        ))}
+        {/* <div className="row">
+          <div className="col-4 text-left">
+            <div className="normal-text text-bold">US</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="normal-text text-bold">$5,000</div>
+          </div>
+          <div className="col-1 text-left">
+            <div className="normal-text">
+              <img src={ArrowRightBlackIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="normal-text text-bold">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="normal-text text-bold">+$5,000</div>
+          </div>
+        </div>
+        <div className="row mt-1">
+          <div className="col-4 text-left">
+            <div className="normal-text ">UK</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="gray-normal-text">$5,000</div>
+          </div>
+          <div className="col-1 text-left">
+            <div className="normal-text">
+              <img src={ArrowRightIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="gray-normal-text">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="gray-normal-text">-</div>
+          </div>
+        </div>
+        <div className="row mt-1">
+          <div className="col-4 text-left">
+            <div className="normal-text ">Canada</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="gray-normal-text">$5,000</div>
+          </div>
+          <div className="col-1 text-left">
+            <div className="normal-text">
+              <img src={ArrowRightIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="gray-normal-text">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="gray-normal-text">-</div>
+          </div>
+        </div> */}
+
+        <div className=" straight-line horizontal-line pt-1 mb-2 " />
+        <div className="row">
+          <div className="col-4 text-left">
+            <div className="normal-text text-bold">Total invoice</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="normal-text text-bold">$5,000</div>
+          </div>
+          <div className="col-1 text-left">
+            <div className="normal-text">
+              <img src={ArrowRightBlackIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="normal-text text-bold">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="normal-text text-bold">+$5,000</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderMobileView = () => {
+    return (
+      <div className="d-md-none d-block">
+        <div className="row">
+          <div className="col-12 text-left">
+            <div className="label">Marketplace</div>
+            <div className="normal-text text-bold">US</div>
+          </div>
+
+          <div className="col-4 text-left">
+            <div className="label">From</div>
+            <div className="normal-text text-bold">$5,000</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="mt-3">
+              <img src={ArrowRightBlackIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">To</div>
+            <div className="normal-text text-bold">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">Change</div>
+            <div className="normal-text text-bold">+$5,000</div>
+          </div>
+        </div>
+        <div className=" straight-line horizontal-line mt-2 mb-2 " />
+        <div className="row">
+          <div className="col-12 text-left">
+            <div className="label">Marketplace</div>
+            <div className="normal-text ">US</div>
+          </div>
+
+          <div className="col-4 text-left">
+            <div className="label">From</div>
+            <div className="gray-normal-text">$5,000</div>
+          </div>
+          <div className="col-2 text-left">
+            <div className="mt-3">
+              <img src={ArrowRightIcon} width="18px" alt="arrow" />{' '}
+            </div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">To</div>
+            <div className="gray-normal-text">$10,000</div>
+          </div>
+          <div className="col-3 text-left">
+            <div className="label">Change</div>
+            <div className="gray-normal-text">+$5,000</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <Modal
+        id={id}
+        isOpen={isOpen}
+        style={{ ...customStyles, ...style }}
+        ariaHideApp={false}
+        contentLabel="Edit modal">
+        <img
+          src={CloseIcon}
+          alt="close"
+          className="float-right cursor cross-icon"
+          onClick={onClick}
+          role="presentation"
+        />
+        <ModalBox>
+          <div className="modal-body pb-0 ">
+            <h4>
+              {' '}
+              <img
+                className="modal-back-arrow"
+                src={LeftArrowIcon}
+                role="presentation"
+                alt="back arrow"
+                onClick={() => onApply()}
+              />
+              Invoice Adjustment
+            </h4>
+            <div className=" straight-line horizontal-line pt-1 mb-2 " />
+            {isMobile ? renderMobileView() : renderDesktopView()}
+
+            <div className=" straight-line horizontal-line mt-2 mb-2 " />
+            <p className="normal-text">
+              The new invoice amount willbe available to spend from December
+              onwards.
+              <br /> The first bill for this amount willbe sent November 13.
+            </p>
+          </div>
+          <div className="footer-line" />
+          <div className="modal-footer">
+            <Button
+              onClick={() => {
+                onApply();
+              }}
+              type="button"
+              className="btn-primary on-boarding   w-100">
+              <img
+                width="16px"
+                className="ml-1"
+                style={{ verticalAlign: 'middle' }}
+                src={BellNotification}
+                alt="bell"
+              />
+              Send Reminder
+            </Button>
+          </div>
+        </ModalBox>
+      </Modal>
+    </>
+  );
+};
+
+export default InvoiceViewAndReminderModal;
+
+InvoiceViewAndReminderModal.defaultProps = {
+  isOpen: false,
+  id: '',
+  style: {},
+  onClick: () => {},
+  onApply: () => {},
+};
+
+InvoiceViewAndReminderModal.propTypes = {
+  isOpen: bool,
+  id: string,
+  style: shape({}),
+  onClick: func,
+  onApply: func,
+};
