bdcd1352e580fb6588f5f715aafff6812834d3d6
aarti singh <aarti.s@amazatic.com>
2021-12-15 12:46:49 +0530

    pdv-3073 worked on modal consistency and align the icon with text.

--- src/common/ErrorMsgBox.js
+++ src/common/ErrorMsgBox.js
@@ -4,17 +4,17 @@ import Theme from '../theme/Theme';
 const ErrorMsgBox = styled.div`
   color: ${Theme.red};
   background-color: #fbf2f2;
-  padding: 9px 13px 11px 13px;
+  padding: 12px 13px 11px 13px;
   border-radius: 4px;
   font-weight: 500;
   text-align: left;
   font-size: ${Theme.extraSmall};
   font-family: ${Theme.baseFontFamily};
-  display: inline-block;
 
   .info-icon {
-    width: 11px;
+    width: 13px;
     margin-right: 2px;
+    vertical-align: text-top;
   }
 `;
 
