9fe56f0392e400da4efec4d4e0d1347c10c7c2d4
Shashank <shashank.n@amazatic.com>
2021-12-23 16:39:22 +0530

    Dummy data integration in progress for role.

---
+++ src/components/Team/dummyData.js
@@ -0,0 +1,58 @@
+const currentTeamMembers = [
+  {
+    id: 'CUxcasd',
+    role_group: {
+      id: 'RGsaio',
+      name: 'BGS Manager',
+    },
+    user: {
+      id: 'USdnasoih',
+      name: 'Troy Dyches',
+      email: 'abc@amazatic.com',
+    },
+  },
+  {
+    id: 'CUxcasd',
+    role_group: {
+      id: 'RGsaio',
+      name: 'BGS',
+    },
+    user: {
+      id: 'USdnasoih',
+      name: 'Marcus Tedesco',
+      email: 'abc@amazatic.com',
+    },
+  },
+  {
+    id: 'CUxcasd',
+    role_group: {
+      id: 'RGsaio',
+      name: 'SPONSORED AD MANAGER',
+    },
+    user: {
+      id: 'USdnasoih',
+      name: 'Neil Nononyana',
+      email: 'abc@amazatic.com',
+    },
+  },
+  {
+    id: 'CUxcasd',
+    role_group: {
+      id: 'RGsaio',
+      name: 'DSP AD MANAGER',
+    },
+    user: {
+      id: 'USdnasoih',
+      name: 'Ben Bradly',
+      email: 'abc@amazatic.com',
+    },
+  },
+  {
+    role_group: {
+      id: 'RGsaio',
+      name: 'CREATIVE',
+    },
+  },
+];
+
+export default currentTeamMembers;
