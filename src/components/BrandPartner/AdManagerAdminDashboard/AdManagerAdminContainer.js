2c773755efec6d3a2e984b9c3deba2b6c4d55b7f
Shashank <shashank.n@amazatic.com>
2022-01-25 19:30:31 +0530

    [Bug]: Uncommeented the commission condition.

--- src/components/BrandPartner/AdManagerAdminDashboard/AdManagerAdminContainer.js
+++ src/components/BrandPartner/AdManagerAdminDashboard/AdManagerAdminContainer.js
@@ -101,14 +101,14 @@ export default function AdManagerAdminContainer({ userInfo }) {
               DSP Advertising
             </li>
 
-            {/* {userInfo && userInfo.role === 'BGS Admin' ? (
+            {userInfo && userInfo.role === 'BGS Admin' ? (
               <li
                 className={viewComponent === 'commissions' ? 'active' : ''}
                 onClick={() => setViewComponent('commissions')}
                 role="presentation">
                 Commissions
               </li>
-            ) : null} */}
+            ) : null}
           </ul>
         </Tabs>
         {renderComponent(viewComponent)}
